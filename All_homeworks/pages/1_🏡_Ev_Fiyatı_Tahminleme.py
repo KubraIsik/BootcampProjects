b864134293c95ff3ebf44d91c872307d4b94a11c
KubraIsik <kuisik@gmail.com>
2023-05-13 20:50:04 +0300

    fix path error

--- All_homeworks/pages/1_🏡_Ev_Fiyatı_Tahminleme.py
+++ All_homeworks/pages/1_🏡_Ev_Fiyatı_Tahminleme.py
@@ -35,7 +35,7 @@ For **PARAMATER EXPLANATIONS**, please go to site: [Input Parameters Explanation
 
 st.sidebar.write(f'### Please select values of the house:')
 
-df1 = pd.read_csv('\dataSets\house_price.csv')
+df1 = pd.read_csv('house_price.csv')
 
 #columns_range_list = functions.df_columns_value_range(df1)
 
@@ -92,7 +92,7 @@ st.write(user_sample)
 
 
 # load the house price model
-filename = '\models\house_price_Model.sav'
+filename = 'house_price_Model.sav'
 house_price_model = pickle.load(open(filename, 'rb'))
 
 prediction = house_price_model.predict(user_sample)
