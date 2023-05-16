fcd255220ddaedf67c0b0af92c4ad1dd6c5c81a9
KubraIsik <kuisik@gmail.com>
2023-05-16 18:37:42 +0300

    Update house price model

--- All_homeworks/pages/1_🏡_Ev_Fiyatı_Tahminleme.py
+++ All_homeworks/pages/1_🏡_Ev_Fiyatı_Tahminleme.py
@@ -5,174 +5,95 @@ import numpy as np
 import pandas as pd
 import pickle
 from PIL import Image
-import sys
-
 
 import functions
 
-st.set_page_config(page_title="Ev Fiyatı Tahminleme", page_icon="🏡")
+st.set_page_config(page_title="Local-House Price", page_icon="🏡")
 
-st.markdown("# 🏡 Ev Fiyatı Tahminleme ")
-st.sidebar.header(" 🏡 Ev Fiyatı Tahminleme ")
-st.write(
-    """**Amaç :** elimizdeki verilere göre eğittiğimiz model ile ev fiyatı tahminleme.
-       \nBurada bir regresyon problemi üzerine çalıştık. 
-       Evin konumu, garaj sayısı gibi birçok özelliğine göre bir evin sahip olabileceği olası değeri tahmin ediyoruz. 
-       \n **👇Aşağıdaki tahminleme aracına istediğiniz değerleri girerek tahmin sonuçlarını 👈yanda görüntüleyebilirsiniz:**
-       \nNOT: Burada predict yaptığımız tüm sütunlar için bir değer almak burayı kalabalık yapar mı? Bazı featureları default alıp
-       kalan featurelar kullanıcıdan input olarak alınsa nasıl olur? predict sonuçlarını etkiler mi?
+st.markdown("# 🏡 Local-House Price")
+st.sidebar.header(" 🏡 Local-House Price")
+st.write("""
+		## Let's Find Out the Price of Your New House !!
+		\n**This project aims** to predict estimated price of a house according to given values can be related with.
+       \nWe worked on a **regression problem** to find a solution.  
+       \n**👈You can use estimation panel which is located on the sidebar to enter feature values of the house**
+       \nFor parameter explanations visit: **[Input Parameters Explanations](https://jse.amstat.org/v19n3/decock/DataDocumentation.txt)**
+	   
        """
 )
 
+st.sidebar.write('### Please select feature values of the house:')
 
-image = Image.open('All_homeworks/pages/houseprice.jpg')
-st.image(image, use_column_width=True)
-
-st.write("""
-## Get the Price for Your New House !!
-	
-This apps predict the **house prices**!
-
-For **PARAMATER EXPLANATIONS**, please go to site: [Input Parameters Explanations](https://jse.amstat.org/v19n3/decock/DataDocumentation.txt)
-
-""")
-
-st.sidebar.write(f'### Please select values of the house:')
-
-
-
-# load dataset
-# with open('house_price_clean2.csv', 'rb') as file:
-#     df1 = pd.read_csv(file)
-
-#df1 = pd.read_csv(f'dataSets/house_price.csv')
+##df1 = pd.read_csv('All_homeworks/dataSets/house_price.csv')
+df1 = pd.read_csv('All_homeworks/dataSets/house_price.csv')
 
 #columns_range_list = functions.df_columns_value_range(df1)
 
 def user_input_features():
-	MSZoning = st.sidebar.selectbox("House zone", (0, 1, 2, 3, 4))
-	LotShape = st.sidebar.selectbox("General shape", (0, 1, 2, 3))
-	HouseStyle = st.sidebar.selectbox("House style", (0, 1, 2, 3, 4, 5, 6, 7))
-	OverallQual = st.sidebar.selectbox("Rates for material and finish house", (2, 3, 4, 5, 6, 7, 8, 9, 10))
-	RoofStyle = st.sidebar.selectbox("Roof style", (0, 1, 2, 3, 4, 5))
-	ExterQual = st.sidebar.selectbox("Exterior material quality", (0,1,2,3))
-	Foundation = st.sidebar.selectbox("Foundation type", (0,1,2,3,4))
-	BsmtQual = st.sidebar.selectbox("Basement height", (0,1,2,3))
-	BsmtExposure = st.sidebar.selectbox("Basement level", (0,1,2,3))
-	HeatingQC = st.sidebar.selectbox("Heating quality", (0,1,2,3,4))
-	BsmtFullBath = st.sidebar.selectbox("Basement full bathroom", (0,1,2))
-	FullBath = st.sidebar.selectbox("Full bathroom", (0,1,2,3))
-	HalfBath = st.sidebar.selectbox("Half bathroom", (0,1,2))
+	MSZoning = st.sidebar.selectbox("House zone", ("RL" ,"RM" ,"FV", "RH" ,"C(all)")) 
+	# RL: Residential Low Density,RM	:Residential Medium Density	
+    # ,FV:	Floating Village Residential, RH	:Residential High Density,  
+    # C(all) : Commercial   
 	BedroomAbvGr = st.sidebar.selectbox("Bedroom", (0,1,2,3,4,5,6))
-	KitchenQual = st.sidebar.selectbox("Kitchen quality", (0,1,2,3))
-	TotRmsAbvGrd = st.sidebar.selectbox("Total rooms above grade", (3,4,5,6,7,8,9,10,11,12))
-	Fireplaces = st.sidebar.selectbox("Number of fireplaces", (0,1,2,3))
-	GarageType = st.sidebar.selectbox("Garage location", (0,1,2,3,4,5))
-	GarageFinish = st.sidebar.selectbox("Interior finish of the garage", (0,1,2))
-	GarageCars = st.sidebar.selectbox("Garage car capacity", (1,2,3,4))
-	SaleCondition = st.sidebar.selectbox("Sale condition", (0,1,2,3,4,5))
+	LotShape = st.sidebar.selectbox("General shape", ("Reg", "IR1" , "IR2"  , "IR3"))
+	 # Reg: Regular	, IR1:	Slightly irregular , IR2:	Moderately Irregular,IR3:	Irregular 
 	LotArea = st.sidebar.slider("Lot size in sqft", 1300, 215245, 5000)
-	Neighborhood = st.sidebar.slider("Neighborhood", 0, 24, 5)
+	OverallQual = st.sidebar.selectbox("Rates for material and finish house", (2, 3, 4, 5, 6, 7, 8, 9, 10))
+	#  1 : Very Poor -  10: Very Excellent
 	YearBuilt = st.sidebar.slider("Construction date", 1880, 2010, 1990)
+	HeatingQC = st.sidebar.selectbox("Heating quality", ("Ex", "TA", "Gd", "Fa", "Po"))
+    #tx1 = st.sidebar.write("Ex: Excellent, Gd:	Good, TA:	Average/Typical, Fa:	Fair, Po:	Poor")
 	YearRemodAdd = st.sidebar.slider("Innovation date", 1950, 2010, 2000)
-	BsmtFinSF1 = st.sidebar.slider("Finished sqft", 0, 5644, 1000)
-	BsmtUnfSF = st.sidebar.slider("Unfinished basement sqft", 0, 2336, 500)
 	TotalBsmtSF = st.sidebar.slider("Total sqft of basement", 105, 6110, 300)
+	onestFlrSF = st.sidebar.slider("First Floor square feet ", 438, 4692, 1098) 
 	GrLivArea = st.sidebar.slider("Living area sqft", 438, 5642, 700)
-	GarageYrBlt = st.sidebar.slider("Garage year built", 1900, 2010, 2000)
+	FullBath = st.sidebar.selectbox("Full bathroom", (0,1,2,3))
+	TotRmsAbvGrd = st.sidebar.selectbox("Total rooms above grade", (3,4,5,6,7,8,9,10,11,12))
+	GarageCars = st.sidebar.selectbox("Garage car capacity", (1,2,3,4))
 	GarageArea = st.sidebar.slider("Garage sqft", 160, 1418, 300)
-	WoodDeckSF = st.sidebar.slider("Wood deck sqft", 0, 857, 300)
-	OpenPorchSF = st.sidebar.slider("Open porch sqft", 0, 547, 100)
-	data = {"MSZoning":MSZoning, "LotShape":LotShape, "HouseStyle":HouseStyle, "OverallQual":OverallQual, "RoofStyle":RoofStyle,
-			"ExterQual":ExterQual, "Foundation":Foundation, "BsmtQual":BsmtQual, "BsmtExposure":BsmtExposure,"HeatingQC":HeatingQC,
-			"BsmtFullBath":BsmtFullBath, "FullBath":FullBath, "HalfBath":HalfBath, "BedroomAbvGr":BedroomAbvGr,"KitchenQual":KitchenQual,
-			"TotRmsAbvGrd":TotRmsAbvGrd, "Fireplaces":Fireplaces, "GarageType":GarageType, "GarageFinish":GarageFinish, "GarageCars":GarageCars,
-			"SaleCondition":SaleCondition, "LotArea":LotArea, "Neighborhood":Neighborhood, "YearBuilt":YearBuilt, "YearRemodAdd":YearRemodAdd,
-			"BsmtFinSF1":BsmtFinSF1, "BsmtUnfSF":BsmtUnfSF, "TotalBsmtSF":TotalBsmtSF, "GrLivArea" : GrLivArea, "GarageYrBlt":GarageYrBlt,
-			"GarageArea":GarageArea, "WoodDeckSF":WoodDeckSF, "OpenPorchSF":OpenPorchSF,
-			}
-	features = pd.DataFrame(data, index=[0])
+	data = {"MSZoning":MSZoning, "BedroomAbvGr":BedroomAbvGr, "LotShape":LotShape, "LotArea":LotArea,
+			"OverallQual":OverallQual, "YearBuilt":YearBuilt, "HeatingQC":HeatingQC, "YearRemodAdd":YearRemodAdd,
+			"TotalBsmtSF":TotalBsmtSF, "1stFlrSF" :onestFlrSF, "GrLivArea" : GrLivArea, "FullBath":FullBath,
+			"TotRmsAbvGrd":TotRmsAbvGrd, "GarageCars":GarageCars, "GarageArea":GarageArea
+	       }
+	features = pd.DataFrame(data,index=[0])
 	return features                     
           
 
 user_sample = user_input_features()
 
-st.subheader("User Input parameters")
-st.write(user_sample)
+with st.container():
+	st.subheader("Selected values of features:")
+	st.write("""\n**👈You can use estimation panel which is located on the sidebar to change feature values of the house**
+				\n**👇Estimated price will be appear below.**""")
+	
+	# CSS to inject contained in a string
+	hide_dataframe_row_index = """
+            <style>
+            .row_heading.level0 {display:none}
+            .blank {display:none}
+            </style>
+            """
 
-# load the house price model
-with open('All_homeworks/pages/house_price_Model.sav', 'rb') as file:
-     house_price_model = pickle.load(file)
-# load the house price model
-# filename = f'models/house_price_Model.sav'
-# house_price_model = pickle.load(open(filename, 'rb'))
-
-prediction = house_price_model.predict(user_sample)
-
-st.subheader('Prediction')
-st.write(prediction)
-
-# Yan tarafta sidebar da görüntüle
-st.success(f"###  👉 Ev fiyatı tahmin: ${prediction}")  
-
-
-# Plot Animation
-# Burada yukarıda hesaplanan prediction değerleri
-# eş zamanlı olarak aşağıdaki plot a eklenip gösterilebilir mi?
-progress_bar = st.sidebar.progress(0)
-status_text = st.sidebar.empty()
-#last_rows = np.random.randn(1, 1)
-last_rows = prediction
-chart = st.line_chart(last_rows)
-
-#for i in range(1, 101):
-if user_sample.any:
-	new_rows = last_rows[-1] + prediction #np.random.randn(5, 1).cumsum(axis=0)
-	#new_rows = prediction
-		#status_text.text("%i%% Complete" % i)
-	chart.add_rows(new_rows)
-		#progress_bar.progress(i)
-	last_rows = new_rows
-time.sleep(0.05)
-
-progress_bar.empty()
-
-# Streamlit widgets automatically run the script from top to bottom. Since
-# this button is not connected to any other logic, it just causes a plain
-# rerun.
-st.button("Re-run")
+	# Inject CSS with Markdown
+	st.markdown(hide_dataframe_row_index, unsafe_allow_html=True)
+	st.table(user_sample)
 
 
-# Plot Animation
-#progress_bar = st.sidebar.progress(0)
-#status_text = st.sidebar.empty()
-#last_rows = np.random.randn(1, 1)
-#chart = st.line_chart(last_rows)
+# target_encoder import and transform
+f = 'All_homeworks/models/Target_Encoder.sav'
+target_encoder = pickle.load(open(f, 'rb'))
+user_sample_encoded= target_encoder.transform(user_sample)
+user_sample_log = np.log1p(user_sample_encoded)
 
-#for i in range(1, 101):
-#    new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
-#    status_text.text("%i%% Complete" % i)
-#    chart.add_rows(new_rows)
-#    progress_bar.progress(i)
-#    last_rows = new_rows
-#    time.sleep(0.05)
-
-#progress_bar.empty()
-
-# Streamlit widgets automatically run the script from top to bottom. Since
-# this button is not connected to any other logic, it just causes a plain
-# rerun.
-#st.button("Re-run")
+filename = 'All_homeworks/models/house_price_model.sav'
+house_price_model = pickle.load(open(filename, 'rb'))
+# predict house
+prediction = house_price_model.predict(user_sample_log)
 
 
-#st.sidebar.write(
-#        f""" \n GrLivArea: Üstü (zemin) oturma alanı metre karesi: {age}
-#            \nMSZoning: Genel imar sınıflandırması: {options}
-#            \nLotFrontage: Mülkiyetin cadde ile doğrudan bağlantısının olup olmaması
-#            \nLotArea: Parsel büyüklüğü
-#            \nStreet: Yol erişiminin tipi
-#            \nAlley: Sokak girişi tipi
-#            \nLotShape: Mülkün genel şekli
-#            \nBsmtQual: Bodrumun yüksekliği
-#            \nYrSold: Satıldığı yıl """
-#)
+# Show Prediction result
+st.success(f"###  👉 Estimated price of this house: ${int(np.round(np.exp(prediction)))}") #  #{float(np.round(prediction,2))}")
+
+image = Image.open('All_homework/images/houseprice.jpg')
+st.image(image, use_column_width=True)
