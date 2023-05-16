8d8b521b85bb863d4786e9f80e4df5dd226d3f91
KubraIsik <kuisik@gmail.com>
2023-05-17 01:22:37 +0300

    En son error çözüldü. Dt_Customer_en çıkarılıp yeniden modellenmeli

--- All_homeworks/pages/2_ 📊_Pazarlama_Kampanyası.py
+++ All_homeworks/pages/2_ 📊_Pazarlama_Kampanyası.py
@@ -7,35 +7,23 @@ from PIL import Image
 from sklearn.preprocessing import StandardScaler
 import pickle
 import sys
+import numpy as np
 
 
-st.set_page_config(page_title="Pazarlama Kampanyası", page_icon="📊")
+st.set_page_config(page_title="Marketing Campaign", page_icon="📊")
 
-st.markdown("# 📊 Pazarlama Kampanyası")
-st.sidebar.header("📊 Pazarlama Kampanyası")
+st.markdown("# 📊 Marketing Campaign")
+st.sidebar.header("📊 Marketing Campaign")
 st.write(
-    """Pazarlama Kampanyası geri dönüşlerini tahmin etmeyi amaçlıyor.
-       \nSınıflandırma modeli üzerinde çalışılmıştır.
-       \nMüşteri profillerine göre pazarlama kampanyasına geri dönüş yapıp yapmamaları sınıflandırılmıştır.
-       \n**👇Aşağıdaki tahminleme aracına istediğiniz sonuçları girerek tahmin sonuçlarını görüntüleyebilirsiniz👈:**"""
+    """## Marketign Campaign is Successful or NOT !!
+        \nThis project aims to predict response of a customer according to marketing campaign features.
+       \nA Classification model is created to estimate response.
+       \n**👈You can use estimation panel on the left to see prediction result👇:**"""
 )
 
-image = Image.open('All_homeworks/pages/marketingcampaign.jpg')
-print(image)
-
-st.image(image, use_column_width=True)
-
-#put the heading
-st.write("""
-## Marketign Campaign is Successful or NOT !!
-	
-This apps predict the **Marketing Campaign Response**!
-
-
-""")
 
 #import dataset
-df = pd.read_csv("All_homeworks/pages/MarketingCampaign.csv")
+df = pd.read_csv("All_homeworks/dataSets/MarketingCampaign.csv")
 
 
 selectbox_list = ['Kidhome', 'Teenhome', 'NumDealsPurchases', 'NumWebPurchases',
@@ -48,12 +36,12 @@ desc_selectbox_list = ["Number of small children in customer’s household",
 						"Number of purchases made through company’s web site",
 						"Number of purchases made using catalogue",
 						"Number of purchases made directly in stores",
-						"Number of visits to company’s web site in the last month"
+						"Number of visits to company’s web site in the last month",
 						"Customer’s level of education",
 						"Customer’s marital status"]
 
 slidebar_list = ['Year_Birth', 'Income', 'Recency', 'MntWines',
-       'MntFruits', 'MntMeatProducts', 'MntFishProducts', 'MntSweetProducts', 'MntGoldProds','Dt_Customer_en']
+       'MntFruits', 'MntMeatProducts', 'MntFishProducts', 'MntSweetProducts', 'MntGoldProds']#,'Dt_Customer_en']
 
 desc_slidebar_list = ["Birthday year", "Customer’s yearly household income", 
 						"Number of days since the last purchase",
@@ -62,8 +50,9 @@ desc_slidebar_list = ["Birthday year", "Customer’s yearly household income",
 						"Amount spent on meat products in the last 2 years",
 						"Amount spent on fish products in the last 2 years",
 						"Amount spent on sweet products in the last 2 years",
-						"Amount spent on gold products in the last 2 years",
-						"Date of customer’s enrolment with the company"]
+						"Amount spent on gold products in the last 2 years"]#,
+						#"Date of customer’s enrolment with the company"]
+                        
 
 box_overall_dict = {}
 slider_overall_dict = {}
@@ -75,8 +64,8 @@ for var1, var2 in zip(selectbox_list, desc_selectbox_list):
 for var1, var2 in zip(slidebar_list, desc_slidebar_list):
     slider_overall_dict.update({var1: var2})
 
-print(box_overall_dict)
-print(slider_overall_dict)
+#st.write(box_overall_dict)
+#st.write(slider_overall_dict)
 
 # Displaying box and slider with functions
 def showing_box(var, desc):
@@ -88,10 +77,6 @@ def showing_slider(var, desc):
         slider = st.sidebar.slider(label= f"{desc}", min_value=round(df[var].min()), max_value=round(df[var].max()))
         return slider
 
-print(selectbox_list)
-print(slidebar_list)
-
-
 # Collecting user inputs in dictionaries
 box_dict = {}
 slider_dict = {}
@@ -102,140 +87,46 @@ for key, value in box_overall_dict.items():
 for key, value in slider_overall_dict.items():
     slider_dict.update({key: showing_slider(key, value)})
 
-print(box_dict)
-print(slider_dict)
-
 
 input_dict = {**box_dict, **slider_dict}
 
+#st.table(input_dict)
+#input_list = [tuple([input_dict['Year_Birth'],input_dict['Income']])] ## bu sekilde bizim X_testimze gore doldurman lazim.
+#print(input_list)
 
-input_list = [tuple([input_dict['Year_Birth'],input_dict['Income']])] ## bu sekilde bizim X_testimze gore doldurman lazim.
-print(input_list)
-
-
+input_list=[input_dict['Year_Birth'], input_dict['Income'],input_dict['Kidhome'],input_dict['Teenhome'],input_dict['Recency'],
+input_dict['MntWines'],input_dict['MntFruits'],input_dict['MntMeatProducts'],input_dict['MntFishProducts'],
+input_dict['MntSweetProducts'],input_dict['MntGoldProds'],input_dict['NumDealsPurchases'],input_dict['NumWebPurchases'],
+input_dict['NumCatalogPurchases'],input_dict['NumStorePurchases'],input_dict['NumWebVisitsMonth'],input_dict['Education_en'],
+input_dict['Marital_Status_en']]
 
 #import model with pickle
-with open('All_homeworks/pages/marketing_model.sav', 'rb') as f:
+with open('All_homeworks/models/marketing_model.sav', 'rb') as f:
     model = pickle.load(f)
 
-#prediction
-print(len(model.feature_names_in_)) 
-print(len(input_list[0]))
-# bu iki sayi birbirine esit olmali .
+#prediction  
+arr_f_name = model.feature_names_in_
 
-predict_df = pd.DataFrame(input_list, columns= model.feature_names_in_)
+columns= arr_f_name[:-1].tolist() # burada -1 yaptım çünkü Dt_Customer_en i almamak için
+# Dt_Customer_en datadan çıkarılıp eklenirse  [:-1] yapmaya gerek yok
 
-print(predict_df)
+
+predict_df = pd.DataFrame([input_list], columns= columns,index=[0])
+st.table(predict_df)
+# üstteki errorlar çözüldü.
+# burada hata veriyor. Çünkü Dt_Customer_en çıkarılıp model train edilip tekrar yüklenmesi gerekiyor
+# Dt_Customer_en ilk başta datadan çıkardığımız bir sürun idi.
+
 ypred = model.predict(predict_df)
-print(ypred)
-st.subheader("Marketing campaign response")
+
+
 st.write(ypred)
+st.subheader("Marketing campaign response")
+# st.write(ypred)
+response = "Yes" if ypred == 0 else "No"
+st.success(f"###  👉 This customer response or not(Yes/No):",response)  
 
+image = Image.open('All_homeworks/images/marketingcampaign.jpg')
+print(image)
+st.image(image, use_column_width=True)
 
-
-# [`st.pydeck_chart`](https://docs.streamlit.io/library/api-reference/charts/st.pydeck_chart)
-
-#st.sidebar.success(f"###  👉 Bu müşteri geri dönüş yapar mı(Evet/Hayır):")  
-
-### Farklı input alma yöntemleri
-#age = st.slider('GrLivArea: Üstü (zemin) oturma alanı metre karesi', 0, 130, 25)
-#st.write("I'm ", age, 'years old')
-
-#values = st.slider(
-#    'MSZoning: Genel imar sınıflandırması',
-#    0.0, 100.0, (25.0, 75.0))
-#st.write('Values:', values)
-
-#options = st.multiselect(
-    #'MSZoning: Genel imar sınıflandırması',
-    #['Evet', 'Yellow', 'Red', 'Blue'],
-    #['Yellow', 'Red'])
-
-#st.write('You selected:', options)
-
-#number = st.number_input('Insert a number')
-#st.write('The current number is ', number)
-
-
-##############################
-# Mapping yapan bir script
-#@st.cache_data
-#def from_data_file(filename):
-#    url = (
-#        "http://raw.githubusercontent.com/streamlit/"
-#        "example-data/master/hello/v1/%s" % filename
-#    )
-#    return pd.read_json(url)
-
-
-#try:
-#    ALL_LAYERS = {
-#        "Bike Rentals": pdk.Layer(
-#            "HexagonLayer",
-#            data=from_data_file("bike_rental_stats.json"),
-#            get_position=["lon", "lat"],
-#            radius=200,
-#            elevation_scale=4,
-#            elevation_range=[0, 1000],
-#            extruded=True,
-#        ),
-#        "Bart Stop Exits": pdk.Layer(
-#            "ScatterplotLayer",
-#            data=from_data_file("bart_stop_stats.json"),
-#            get_position=["lon", "lat"],
-#            get_color=[200, 30, 0, 160],
-#            get_radius="[exits]",
-#            radius_scale=0.05,
-#        ),
-#        "Bart Stop Names": pdk.Layer(
-#            "TextLayer",
-#            data=from_data_file("bart_stop_stats.json"),
-#            get_position=["lon", "lat"],
-#            get_text="name",
-#            get_color=[0, 0, 0, 200],
-#            get_size=15,
-#            get_alignment_baseline="'bottom'",
-#        ),
-#        "Outbound Flow": pdk.Layer(
-#            "ArcLayer",
-#            data=from_data_file("bart_path_stats.json"),
-#            get_source_position=["lon", "lat"],
-#            get_target_position=["lon2", "lat2"],
-#            get_source_color=[200, 30, 0, 160],
-#            get_target_color=[200, 30, 0, 160],
-#            auto_highlight=True,
-#            width_scale=0.0001,
-#            get_width="outbound",
-#            width_min_pixels=3,
-#            width_max_pixels=30,
-#        ),
-#    }
-#    st.sidebar.markdown("### Map Layers")
-#    selected_layers = [
-#        layer
-#        for layer_name, layer in ALL_LAYERS.items()
-#        if st.sidebar.checkbox(layer_name, True)
-#    ]
-#    if selected_layers:
-#        st.pydeck_chart(
-#            pdk.Deck(
-#                map_style="mapbox://styles/mapbox/light-v9",
-#                initial_view_state={
-#                    "latitude": 37.76,
-#                    "longitude": -122.4,
-#                    "zoom": 11,
-#                    "pitch": 50,
-#                },
-#                layers=selected_layers,
-#            )
-#        )
-#    else:
-#        st.error("Please choose at least one layer above.")
-#except URLError as e:
-#    st.error(
-#        """
-#        **This demo requires internet access.**
-#        Connection error: %s
-#    """
-#        % e.reason
-#    )
