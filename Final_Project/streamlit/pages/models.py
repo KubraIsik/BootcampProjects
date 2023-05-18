1561a14aeb6da99288e36663d0abbcf3c32769f1
Hamit Güner <hamit.gunr@gmail.com>
2023-05-18 15:09:27 +0300

    final project streamlit

---
+++ Final_Project/streamlit/pages/models.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👺",
+)
+
+
+st.write("# Welcome to Streamlit! 👋")
+
+st.sidebar.success("Select a demo above.")
